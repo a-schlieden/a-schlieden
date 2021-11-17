92e28206730f94271abe5ca49d597dbf2e7ec344
Alexander <93073947+a-schlieden@users.noreply.github.com>
2021-11-17 20:34:52 +0100

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,87 @@
+
+const bgCollektion = ['#ff4f81', '#ff6c5f', '#0077c8', '#74d2e7', '#fd5c63', '#a71930', '#a51890', '#ffc845', '#ff6319'];
+const languageArr = {
+    "title": {
+        "ru": "мой сайт-визитка",
+        "en": "site about me",
+        "de": "die Site über mich",
+    },
+    "i": {
+        "ru": "Я",
+        "en": "I am",
+        "de": "Ich bin",
+    },
+    "hobby": {
+        "ru": "Хобби",
+        "en": "My hobby",
+        "de": "Meine Hobbys",
+    },
+    "work": {
+        "ru": "Мои работы",
+        "en": "My works",
+        "de": "Meine Arbeiten",
+    },
+    "skills": {
+        "ru": "Навыки",
+        "en": "My Skills",
+        "de": "Meine Qualifikationen",
+    },
+    "info": {
+        "ru": "Меня зовут Александр. Мне 33 года. Я родился и вырос в Украине. Женат, есть дочь. С 2019 года мы переехали в Германию в город Лейпциг где сейчас и проживаем.",
+        "en": "My name is Alexander. I am 33 years old. I was born and raised in Ukraine. He is married and has a daughter. Since 2019, we have moved to Germany, to the city of Leipzig, where we now live.",
+        "de": "Ich heiße Alexander. Ich bin 33 jahre alt Ich wurde in der Ukraine geboren, bin verheiratet und habe eine Tochter. Seit 2019 Wonen wir in Deutschland",
+    },
+    "projekts": {
+        "ru": "Здесь можно посмотреть мои проекты.",
+        "en": "",
+        "de": "Hier kann man Projekte sehen, die ich schon gemacht habe.",
+    },
+    "internet": {
+        "ru": "Интернет-провайдер",
+        "en": "Internet provider",
+        "de": "Internetdienstanbieter",
+    },
+    "anime": {
+        "ru": "Аниме",
+        "en": "Anime",
+        "de": "Anime",
+    },
+    "skooter": {
+        "ru": "Scootersnchairs",
+        "en": "Scootersnchairs",
+        "de": "Scootersnchairs",
+    },
+    "favorite": {
+        "ru": "Я увлекаюсь рыбалкой. А еще мы всей семьей с удовольствием путешествуем и открываем для себя новые города и страны",
+        "en": "",
+        "de": "Ich mag Angeln. Ausserdem.... ",
+    },
+    "skillsdeskription": {
+        "ru": "Навыки, которые я приобрел в процессе обучения и выполнения работ.",
+        "en": "My skills.",
+        "de": "",
+    },
+    "hello": {
+        "morning": {
+            "ru": "Доброе утро",
+            "en": "Good day",
+            "de": "Guten Morgen",
+        },
+        "day": {
+            "ru": "Добрый день",
+            "en": "Good day",
+            "de": "Guten Tag",
+        },
+        "evning": {
+            "ru": "Добрый вечер",
+            "en": "Good day",
+            "de": "Guten Abend",
+        },
+        "night": {
+            "ru": "Доброй ночи",
+            "en": "Good day",
+            "de": "Gute Nacht",
+        }
+
+    }
+}
